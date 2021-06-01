9ff24f41f13d77df4ec255db163464d433987eaf
jackteruya <72140204+jackteruya@users.noreply.github.com>
2021-06-01 14:30:36 -0400

    Update painel_mrbit.py
    Atualização painel mrbit incluido altualização de dados

--- painel_mrbit.py
+++ painel_mrbit.py
@@ -1,17 +1,15 @@
-import datetime
-
 import requests
 import time
 
-url = 'https://min-api.cryptocompare.com/data/pricemulti?fsyms=BTC,ETH,LTC&tsyms=USD,EUR'
-
-data = requests.get(url=url)
-data = data.json()
-btc = data['BTC']['USD']
-eth = data['ETH']['USD']
-ltc = data['LTC']['USD']
-
-mensage = f'BTC: $ {btc} \nETH: $ {eth} \nLTC: $ {ltc}'
-print(mensage)
-print(datetime.datetime(hour=24))
-
+while True:
+    url = 'https://min-api.cryptocompare.com/data/pricemulti?fsyms=BTC,ETH,LTC&tsyms=USD,EUR'
+    data = requests.get(url=url)
+    data = data.json()
+    btc = data['BTC']['USD']
+    eth = data['ETH']['USD']
+    ltc = data['LTC']['USD']
+    mensage = f'BTC: $ {btc} \nETH: $ {eth} \nLTC: $ {ltc}'
+    print('### Paienl ###')
+    print(mensage)
+    print('\n')
+    time.sleep(100)
